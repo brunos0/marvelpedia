7ca8acf6babd4172ab850c1c655e56cfa6f1f563
Bruno Horvat <brunohorvat@Mac-MCC-C02G.local>
2024-01-22 16:12:40 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/pocketpedia/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pocketpedia
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Custom Clipper')),
+        body: Stack(children: [
+          Center(
+              child: ClipPath(
+            clipper: MyCustomClipper(),
+            child: Container(
+                height: 200,
+                width: 380,
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Color(0xFFFCF47C),
+                      Color(0xFFBC8905),
+                    ],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  ),
+                )),
+          )),
+          Positioned(
+              top: 280,
+              left: 10,
+              child: Image.asset(
+                'assets/pikachu.png',
+                height: 200,
+                width: 200,
+              ))
+        ]),
+      ),
+    ),
+  );
+}
+
+class MyCustomClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    var radius = 40.0; // Ajuste o valor para alterar a curvatura
+
+    path.moveTo(0, size.height - size.height * 0.75 + radius + 20);
+    path.quadraticBezierTo(0, size.height - size.height * 0.75 + 20, radius,
+        size.height - size.height * 0.78 + 20);
+    path.lineTo(size.width - radius, 0);
+    path.quadraticBezierTo(size.width, 0, size.width, radius);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
