e9ac694d6ccaa0b816181ab480ff8087f81dc9e8
Bruno Horvat <brunohorvat@Mac-MCC-C02G.local>
2024-01-23 17:35:48 -0300

    improve pokemon tile

--- lib/features/pokemon/presentation/widgets/pokemon_clip.dart
+++ lib/features/pokemon/presentation/widgets/pokemon_clip.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pocketpedia/utils/app_routes.dart';
 import 'package:pocketpedia/utils/color_picker.dart';
+import 'package:pocketpedia/utils/string_extensions.dart';
 
 class PokemonClip extends StatelessWidget {
   const PokemonClip(
@@ -12,61 +13,136 @@ class PokemonClip extends StatelessWidget {
   final String pokemonNumber;
   final String pokemonName;
   final List<String> pokemonTypes;
+
+  List listTypes(List<String> list) {
+    List tiles = [];
+    for (int i = 0; i < list.length; i++) {
+      tiles.add(
+        ClipPath(
+          clipper: PKClipper(),
+          child: Container(
+            height: 20,
+            width: 60,
+            color: colorTypePicker(list[i]),
+            // TODO(bruno): icon
+            child: Center(
+                child: Text(
+              list[i].split(' ').map((word) => word.capitalize()).join(' '),
+              style: const TextStyle(
+                fontSize: 10,
+                color: Colors.white,
+              ),
+            )),
+          ),
+        ),
+      );
+      tiles.add(const Spacer(flex: 1));
+    }
+    return tiles;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: ClipPath(
-        clipper: MyCustomClipper(),
-        child: Container(color: colorTypeBackgroundPicker(pokemonTypes[0])),
-
-        //Container(color: colorTypePicker(pokemonTypes[0])),
-      ),
-      /*
-      ClipRRect(
-        borderRadius: BorderRadius.circular(10),
-        child: GridTile(
-          /*
-          footer: GridTileBar(
-            backgroundColor: Colors.black87,
-            title: Text(
-              pokemonName,
-              textAlign: TextAlign.center,
-            ),
-          ),
-          */
-
-          child: GestureDetector(
-            child: Hero(
-              tag: pokemonName,
-              child: FadeInImage(
-                placeholder:
-                    // TODO(bruno): se pikachu então imagem local
-                    NetworkImage(
-                        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/$pokemonNumber.png'),
-                image: NetworkImage(
-                    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/$pokemonNumber.png'),
-                fit: BoxFit.cover,
+        clipper: PKClipper(),
+        child: Container(
+          color: colorTypeBackgroundPicker(pokemonTypes[0]),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 30, bottom: 10),
+                child: SizedBox(
+                  width: 130,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Spacer(),
+                      Text(
+                        '#${pokemonNumber.padLeft(3, '0')}',
+                        style: TextStyle(fontSize: 14, fontFamily: 'ROBOTO'),
+                      ),
+                      FittedBox(
+                        fit: BoxFit.fill,
+                        child: Text(
+                          pokemonName
+                              .split(' ')
+                              .map((word) => word.capitalize())
+                              .join(' '),
+                          style: const TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'ROBOTO'),
+                        ),
+                      ),
+                      const Spacer(),
+                      Row(children: [...listTypes(pokemonTypes)]),
+                      const Spacer(),
+                    ],
+                  ),
+                ),
               ),
-            ),
-            onTap: () {
-              /*
-              Navigator.of(context).pushNamed(
-                AppRoutes.movieDetail,
-                arguments: (movieName, movieDescription, moviePoster),
-              );
-              */
-            },
+              SizedBox(
+                width: 190,
+                child: Container(
+                  // color: Colors.red,
+                  child: Stack(
+                    children: [
+                      Opacity(
+                        opacity: 0.1,
+                        child: Align(
+                          alignment: Alignment.centerRight,
+                          child: Image.asset('assets/pokeball.png',
+                              height: 140,
+                              width: 140,
+                              alignment: Alignment.centerRight,
+                              colorBlendMode: BlendMode.modulate),
+                        ),
+                      ),
+                      // TODO(bruno): se pikachu então imagem local
+                      GestureDetector(
+                          child: Hero(
+                            tag: pokemonName,
+                            child: Align(
+                              alignment: Alignment.bottomLeft,
+                              child: FadeInImage(
+                                alignment: Alignment.bottomLeft,
+                                height: 100,
+                                width: 100,
+                                placeholder:
+
+                                    // TODO(bruno): se pikachu então imagem local
+                                    NetworkImage(
+                                        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/$pokemonNumber.png'),
+                                image: NetworkImage(
+                                    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/$pokemonNumber.png'),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          onTap: () {
+                            /*
+                          Navigator.of(context).pushNamed(
+                            AppRoutes.movieDetail,
+                            arguments: (movieName, movieDescription, moviePoster),
+                          );
+                          */
+                          }),
+                    ],
+                  ),
+                ),
+              )
+            ],
           ),
         ),
       ),
-
-      */
     );
   }
 }
 
-class MyCustomClipper extends CustomClipper<Path> {
+class PKClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     var path = Path();
