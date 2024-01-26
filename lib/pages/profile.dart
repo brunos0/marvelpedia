cb39860a4618c16694a65683ad70ae2eb06e0442
Bruno Horvat <brunohorvat@Mac-MCC-C02G.local>
2024-01-26 11:07:24 -0300

    profile done

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:pocketpedia/core/constants/string.dart';
 
 class ProfileDisplay extends StatefulWidget {
   const ProfileDisplay({super.key});
@@ -10,11 +12,54 @@ class ProfileDisplay extends StatefulWidget {
 class _ProfileDisplayState extends State<ProfileDisplay> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Column(
-        children: <Widget>[
-          Text('teste'),
-        ],
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 60,
+              backgroundColor: Colors.grey,
+              backgroundImage: Image.asset('assets/profile_pic.jpeg').image,
+            ),
+            const Gap(20),
+            const Text(
+              Strings.name,
+              style: TextStyle(fontSize: 24),
+            ),
+            const Text(Strings.role, style: TextStyle(fontSize: 20)),
+            const Gap(20),
+            const Text(Strings.about_title, style: TextStyle(fontSize: 20)),
+            const Text(Strings.about_description,
+                style: TextStyle(fontSize: 14)),
+            const Gap(20),
+            const Text(Strings.about_project_title,
+                style: TextStyle(fontSize: 20)),
+            const Text(
+              Strings.about_project_description,
+              style: TextStyle(fontSize: 14),
+              textAlign: TextAlign.justify,
+            ),
+            const Gap(20),
+            const Text(Strings.about_contact_title,
+                style: TextStyle(fontSize: 20)),
+            const Text(Strings.about_contact_description1,
+                style: TextStyle(fontSize: 14)),
+            const Text(Strings.about_contact_description2,
+                style: TextStyle(fontSize: 14)),
+            const Gap(20),
+            const Text(Strings.about_copyright_title,
+                style: TextStyle(fontSize: 20)),
+            const Text(
+              Strings.about_copyright_description,
+              style: TextStyle(
+                fontSize: 14,
+              ),
+              textAlign: TextAlign.justify,
+            )
+          ],
+        ),
       ),
     );
   }
