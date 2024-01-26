e9e8a6477bc6878b138e14b3d0f4622352d578d3
Bruno Horvat <brunohorvat@Mac-MCC-C02G.local>
2024-01-26 11:23:52 -0300

    profile done

--- lib/core/constants/string.dart
+++ lib/core/constants/string.dart
@@ -2,15 +2,16 @@ class Strings {
   static const String name = 'Bruno Horvat';
   static const String role = 'Flutter Developer';
   static const String about_title = 'About me';
-  static const String about_description = 'blah blah bla';
+  static const String about_description =
+      '''I'm a creative Flutter programmer and passionate about technology. I love exploring new technology trends and using my creativity to solve complex problems. Always looking for learning and growth.''';
   static const String about_project_title = 'About project';
   static const String about_project_description =
       'The application was developed with the aim of training the concepts of Hive, Bloc, Dependency Injection, consumption of Apis Rest from Flutter.';
   static const String about_contact_title = 'Contact';
   static const String about_contact_description1 =
-      'Linkedin: https://www.linkedin.com/in/bruno-horvat/';
+      ' https://www.linkedin.com/in/bruno-horvat/';
   static const String about_contact_description2 =
-      'GitHub: https://github.com/brunos0';
+      ' https://github.com/brunos0';
   static const String about_copyright_title = 'Copyright';
   static const String about_copyright_description =
       '''All images contained in this application are \nthe property of Nintendo.\n'''
--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -12,54 +12,72 @@ class ProfileDisplay extends StatefulWidget {
 class _ProfileDisplayState extends State<ProfileDisplay> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            CircleAvatar(
-              radius: 60,
-              backgroundColor: Colors.grey,
-              backgroundImage: Image.asset('assets/profile_pic.jpeg').image,
-            ),
-            const Gap(20),
-            const Text(
-              Strings.name,
-              style: TextStyle(fontSize: 24),
-            ),
-            const Text(Strings.role, style: TextStyle(fontSize: 20)),
-            const Gap(20),
-            const Text(Strings.about_title, style: TextStyle(fontSize: 20)),
-            const Text(Strings.about_description,
-                style: TextStyle(fontSize: 14)),
-            const Gap(20),
-            const Text(Strings.about_project_title,
-                style: TextStyle(fontSize: 20)),
-            const Text(
-              Strings.about_project_description,
-              style: TextStyle(fontSize: 14),
-              textAlign: TextAlign.justify,
-            ),
-            const Gap(20),
-            const Text(Strings.about_contact_title,
-                style: TextStyle(fontSize: 20)),
-            const Text(Strings.about_contact_description1,
-                style: TextStyle(fontSize: 14)),
-            const Text(Strings.about_contact_description2,
-                style: TextStyle(fontSize: 14)),
-            const Gap(20),
-            const Text(Strings.about_copyright_title,
-                style: TextStyle(fontSize: 20)),
-            const Text(
-              Strings.about_copyright_description,
-              style: TextStyle(
-                fontSize: 14,
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          CircleAvatar(
+            radius: 60,
+            backgroundColor: Colors.grey,
+            backgroundImage: Image.asset('assets/profile_pic.jpeg').image,
+          ),
+          const Gap(20),
+          const Text(
+            Strings.name,
+            style: TextStyle(fontSize: 24),
+          ),
+          const Text(Strings.role, style: TextStyle(fontSize: 20)),
+          const Gap(20),
+          const Text(Strings.about_title, style: TextStyle(fontSize: 20)),
+          const Text(Strings.about_description, style: TextStyle(fontSize: 14)),
+          const Gap(20),
+          const Text(Strings.about_project_title,
+              style: TextStyle(fontSize: 20)),
+          const Text(
+            Strings.about_project_description,
+            style: TextStyle(fontSize: 14),
+            textAlign: TextAlign.justify,
+          ),
+          const Gap(20),
+          const Text(Strings.about_contact_title,
+              style: TextStyle(fontSize: 20)),
+          Row(
+            children: [
+              Image.asset(
+                'assets/in.png',
+                width: 20,
+                height: 20,
               ),
-              textAlign: TextAlign.justify,
-            )
-          ],
-        ),
+              const Text(Strings.about_contact_description1,
+                  style: TextStyle(fontSize: 14)),
+            ],
+          ),
+          Row(
+            children: [
+              Image.asset(
+                'assets/git.jpeg',
+                width: 20,
+                height: 20,
+              ),
+              const Text(
+                Strings.about_contact_description2,
+                style: TextStyle(fontSize: 14),
+                textAlign: TextAlign.left,
+              ),
+            ],
+          ),
+          const Gap(20),
+          const Text(Strings.about_copyright_title,
+              style: TextStyle(fontSize: 20)),
+          const Text(
+            Strings.about_copyright_description,
+            style: TextStyle(
+              fontSize: 14,
+            ),
+            textAlign: TextAlign.justify,
+          )
+        ],
       ),
     );
   }
